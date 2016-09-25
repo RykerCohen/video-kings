29ccdd1d2a8a4b2c3a910c3832260d2920e35594
danielstpaul <danielstpaul@hotmail.com>
2016-09-25 21:24:29 +0100

    build v2.3

--- app/scripts/routes.js
+++ app/scripts/routes.js
@@ -34,6 +34,22 @@ angular.module('Routes', [
         templateUrl: 'views/projects.html',
         controller: 'ProjectsCtrl'
       })
+      .when('/music', {
+        templateUrl: 'views/music.html',
+        controller: 'WorkCtrl'
+      })
+      .when('/commercial', {
+        templateUrl: 'views/commercial.html',
+        controller: 'WorkCtrl'
+      })
+      .when('/interviews', {
+        templateUrl: 'views/interviews.html',
+        controller: 'WorkCtrl'
+      })
+      .when('/features', {
+        templateUrl: 'views/features.html',
+        controller: 'WorkCtrl'
+      })
       .otherwise({
         redirectTo: '/'
       });
