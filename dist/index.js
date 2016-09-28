55497ff368cbb82423ca88c72467a7a88dc07021
danielstpaul <danielstpaul@hotmail.com>
2016-09-28 18:07:09 +0100

    poo

--- dist/index.js
+++ dist/index.js
@@ -6,18 +6,8 @@ var http = require('http');
 // var logger = require('morgan');
 
 var app = express();
-// app.use(logger());
-// app.use(gzippo.staticGzip('' + __dirname));
-
-// serve angular front end files from root path
-router.use('/', express.static('app', { redirect: false }));
-
-// rewrite virtual urls to angular app to enable refreshing of internal pages
-router.get('*', function (req, res, next) {
-  res.sendFile(path.resolve('index.html'));
-});
-
-module.exports = router;
+app.use(logger());
+app.use(gzippo.staticGzip('' + __dirname));
 
 var server = http.createServer(app);
 server.listen(process.env.PORT || 5000);
